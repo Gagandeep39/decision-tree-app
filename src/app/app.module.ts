daa64c379d699ac142e23f2b4505cba89c570c08
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-07-16 21:21:00 +0530

    Added root styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { DataViewComponent } from './data-view/data-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent,
+    DataViewComponent
   ],
   imports: [
     BrowserModule,
