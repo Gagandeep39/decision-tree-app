25fa0145225433330cfa799f7662967dee29406c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-07-17 11:46:17 +0530

    Added basic upload logic

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FileProcessService } from '../services/file-process.service';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.scss']
+  styleUrls: ['./file-upload.component.scss'],
 })
 export class FileUploadComponent implements OnInit {
+  nameOfFile: string = '';
 
-  constructor() { }
+  constructor(private fileProcessService: FileProcessService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // Validate input file type and send for processing
+  onFileChange(event: any) {
+    this.nameOfFile = event.target.files[0].name;
+
+    // Validate File Input Type
+    if (
+      !(this.nameOfFile.endsWith('.xlsx') || this.nameOfFile.endsWith('.xls'))
+    )
+      alert('File type not supported');
+    // Send for Processing
+    else this.fileProcessService.readInputFile(event.target.files[0]);
   }
-
 }
