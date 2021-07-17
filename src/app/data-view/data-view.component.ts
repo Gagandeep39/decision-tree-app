aae4b8d4e3c4571fccd2812db139e178dd330fa6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-07-17 12:10:00 +0530

    Added tble to view data

--- src/app/data-view/data-view.component.ts
+++ src/app/data-view/data-view.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { InputData } from '../models/input-data.model';
+import { FileProcessService } from '../services/file-process.service';
 
 @Component({
   selector: 'app-data-view',
   templateUrl: './data-view.component.html',
-  styleUrls: ['./data-view.component.scss']
+  styleUrls: ['./data-view.component.scss'],
 })
 export class DataViewComponent implements OnInit {
+  fileItems!: BehaviorSubject<InputData[]>;
 
-  constructor() { }
+  constructor(private fileProcessService: FileProcessService) {}
 
   ngOnInit(): void {
+    this.fetchData();
   }
 
+  fetchData() {
+    this.fileItems = this.fileProcessService.result;
+  }
 }
